fedb5368d519ad0eb878a26c3e933caeef6122af
Ric <richardgregson1989@gmail.com>
2023-12-28 15:32:38 +0200

    company & user class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('hi there')
+/// <reference types="@types/google.maps" />
+import { User } from './User'
+import { Company } from './Company'
+
+const user = new User();
+console.log(user)
+const company = new Company()
+console.log(company)
